8a914c1acff22f975db9ec8c0f611f8d74389c24
raelulu <raelulu23@gmail.com>
2023-05-07 22:18:18 +0900

    feat:05.07

---
+++ Section26/ScoreKeeper/app.js
@@ -0,0 +1,28 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+
+let p1Score = 0;
+let p2Score = 0;
+let winnigScore = 5;
+let isGameOver = false;
+
+p1Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p1Score += 1;
+    if (p1Score === winnigScore) {
+      isGameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
+});
+p2Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winnigScore) {
+      isGameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+});
