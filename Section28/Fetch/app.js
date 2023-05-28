68021385763b0d4d6ef3694b64c88454c8447a8b
raelulu <raelulu23@gmail.com>
2023-05-28 22:55:37 +0900

    feat:05.28

--- Section28/Fetch/app.js
+++ Section28/Fetch/app.js
@@ -1,14 +1,14 @@
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("RESOLVED!", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.log("ERROR!", e);
-  });
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     console.log("RESOLVED!", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
 
 // fetch("https://swapi.dev/api/people/1/")
 //   .then((res) => {
