0dbbabb63975c75c28f525146ccc7fc33b9308cf
raelulu <raelulu23@gmail.com>
2023-05-29 22:33:41 +0900

    feat:05.29

--- Section28/Axios/app.js
+++ Section28/Axios/app.js
@@ -7,14 +7,37 @@
 //     console.log("ERROR! ", e);
 //   });
 
-const getStarWarsPerson = async (id) => {
+// const getStarWarsPerson = async (id) => {
+//   try {
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//     console.log(res.data);
+//   } catch (e) {
+//     console.log("ERROR", e);
+//   }
+// };
+
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+const getDadJoke = async () => {
   try {
-    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-    console.log(res.data);
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get(
+      "https://icanhazdadjxcvoke.com/?type=json",
+      config
+    );
+    return res.data.joke;
   } catch (e) {
-    console.log("ERROR", e);
+    return "NO JOKES AVAILABLE! SORRY :(";
   }
 };
-
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+button.addEventListener("click", addNewJoke);
