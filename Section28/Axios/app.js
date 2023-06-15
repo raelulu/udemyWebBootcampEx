85d5318d8fadd258c421c3712335d887210e9ca2
raelulu <raelulu23@gmail.com>
2023-06-15 14:12:40 +0900

    feat: axios연습

--- Section28/Axios/app.js
+++ Section28/Axios/app.js
@@ -31,10 +31,7 @@ const addNewJoke = async () => {
 const getDadJoke = async () => {
   try {
     const config = { headers: { Accept: "application/json" } };
-    const res = await axios.get(
-      "https://icanhazdadjxcvoke.com/?type=json",
-      config
-    );
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
     return res.data.joke;
   } catch (e) {
     return "NO JOKES AVAILABLE! SORRY :(";
