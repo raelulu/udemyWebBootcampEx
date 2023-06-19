ca87d38e326e8e25a51bf50f84e7b2b93c51870d
raelulu <raelulu23@gmail.com>
2023-06-19 20:13:58 +0900

    feat: 복습

--- Section28/TVShowSearch/app.js
+++ Section28/TVShowSearch/app.js
@@ -1,19 +1,24 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", async function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const searchTerm = form.elements.query.value;
-  const config = { params: { q: searchTerm } };
-  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-  makeImages(res.data);
-  form.elements.query.value = "";
+  console.log("SUBMITTED!");
 });
 
-const makeImages = (shows) => {
-  for (let result of shows) {
-    if (result.show.image) {
-      const img = document.createElement("IMG");
-      img.src = result.show.image.medium;
-      document.body.append(img);
-    }
-  }
-};
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
+//   const searchTerm = form.elements.query.value;
+//   const config = { params: { q: searchTerm } };
+//   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+//   makeImages(res.data);
+//   form.elements.query.value = "";
+// });
+
+// const makeImages = (shows) => {
+//   for (let result of shows) {
+//     if (result.show.image) {
+//       const img = document.createElement("IMG");
+//       img.src = result.show.image.medium;
+//       document.body.append(img);
+//     }
+//   }
+// };
