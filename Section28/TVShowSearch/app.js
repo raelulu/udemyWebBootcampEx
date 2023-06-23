37ad5646e3234fe811f074901934e073b75a9bf3
raelulu <raelulu23@gmail.com>
2023-06-23 18:04:02 +0900

    feat: api 요청연습

--- Section28/TVShowSearch/app.js
+++ Section28/TVShowSearch/app.js
@@ -1,24 +1,31 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
-  console.log("SUBMITTED!");
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`,
+    config
+  );
+  makeImages(res.data);
+  form.elements.query.value = "";
 });
 
-// form.addEventListener("submit", async function (e) {
-//   e.preventDefault();
-//   const searchTerm = form.elements.query.value;
-//   const config = { params: { q: searchTerm } };
-//   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-//   makeImages(res.data);
-//   form.elements.query.value = "";
-// });
+const makeImages = (shows) => {
+  const div = document.createElement("div");
+  div.id = "div";
+  document.body.append(div);
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      img.id = "image";
+      document.getElementById("div").appendChild(img);
+    }
+  }
+};
 
-// const makeImages = (shows) => {
-//   for (let result of shows) {
-//     if (result.show.image) {
-//       const img = document.createElement("IMG");
-//       img.src = result.show.image.medium;
-//       document.body.append(img);
-//     }
-//   }
-// };
+const delImages = () => {
+  const div = document.getElementById("div");
+  document.body.removeChild(div);
+};
