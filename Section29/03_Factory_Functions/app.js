a6f79ccdcf4b4505942b129046f895c410cec5ba
raelulu <raelulu23@gmail.com>
2023-06-26 22:37:01 +0900

    feat: 팩토리함수와 생성자함수 공부

--- Section29/03_Factory_Functions/app.js
+++ Section29/03_Factory_Functions/app.js
@@ -1,27 +1,60 @@
-//This functions makes and returns an object every time it is called.
-// The resulting objects all follow the same "recipe"
-function makeColor(r, g, b) {
-	const color = {};
-	color.r = r;
-	color.g = g;
-	color.b = b;
-	color.rgb = function() {
-		const { r, g, b } = this;
-		return `rgb(${r}, ${g}, ${b})`;
-	};
-	color.hex = function() {
-		const { r, g, b } = this;
-		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-		);
-	};
-	return color;
+// //This functions makes and returns an object every time it is called.
+// // The resulting objects all follow the same "recipe"
+
+// function rgb(r, g, b) {
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// // hex(255, 100, 25);
+// // rgb(255, 100, 25);
+// // "#ff6419"
+// // "rgb(255,100,25)"
+
+// function makeColor(r, g, b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+//   return color;
+// }
+
+// const firstColor = makeColor(35, 255, 150);
+// firstColor.hex(); //firstColor.hex()
+// firstColor.rgb(); //"rgb(35,255,150)"
+
+// const black = makeColor(0, 0, 0);
+// black.rgb(); //"rgb(0,0,0)"
+// black.hex(); //"#0000s00"
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
 }
 
-const firstColor = makeColor(35, 255, 150);
-firstColor.hex(); //firstColor.hex();
-firstColor.rgb(); //"rgb(35, 255, 150)"
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
-const black = makeColor(0, 0, 0);
-black.rgb(); //"rgb(0, 0, 0)"
-black.hex(); //"#0000s00"
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+  const { r, g, b } = this;
+  return `rgba(${r}, ${g},${b},${a})`;
+};
+
+const color1 = new Color(40, 255, 60);
+const color2 = new Color(0, 0, 0);
+("rgb(40, 255, 60)");
