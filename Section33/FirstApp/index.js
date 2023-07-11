a14c2423d7f0223a4aee4d45ffb9e4d2da116a06
raelulu <raelulu23@gmail.com>
2023-07-11 19:10:37 +0900

    feat: express,templating 공부

--- Section33/FirstApp/index.js
+++ Section33/FirstApp/index.js
@@ -6,6 +6,26 @@ const app = express();
 //   res.send("<h1>This is my webpage!</h1>");
 // });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page!");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`);
+});
+
+app.get("/r/:subreddit/:postId", (req, res) => {
+  const { subreddit, postId } = req.params;
+  res.send(
+    `<h1>Viewing Post ID: ${postId} on the Browsing the ${subreddit} subreddit</h1>`
+  );
+});
+
+app.post("/cats", (req, res) => {
+  res.send("POST REQUSET TO /cats!!! THIS IS DIFFERENT THAN A GET REQUEST!");
+});
+
 app.get("/cats", (req, res) => {
   res.send("MEOW!!");
 });
@@ -13,6 +33,20 @@ app.get("/cats", (req, res) => {
 app.get("/dogs", (req, res) => {
   res.send("WOOF!");
 });
+
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    res.send("NOTHING FOUND IF NOTHING SEARCHED!");
+  }
+  res.send(`<h1>Search results for: ${q}</h1>`);
+});
+
+app.get("*", (req, res) => {
+  res.send(`I don't know that path!`);
+});
+
+// /r/SOMETHINGHERE
 
 // /cats => 'meow'
 // /dogs => 'woof'
