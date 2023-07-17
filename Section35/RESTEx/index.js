6138d0d539b9910b8353ef14a4cefb2e982573e8
raelulu <raelulu23@gmail.com>
2023-07-17 17:43:26 +0900

    feat: handle JSON,urlencoded data

--- Section35/RESTEx/index.js
+++ Section35/RESTEx/index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos response");
 });
 
 app.post("/tacos", (req, res) => {
-  console.log(req.body);
-  res.send("POST /tacos response");
+  const { meat, qty } = req.body;
+  res.send(`OK, chere are your ${qty} ${meat} tacos`);
 });
 
 app.listen(3000, () => {
