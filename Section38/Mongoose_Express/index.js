79000907b5933dc707e58b5ae0d29ec451d70ccc
raelulu <raelulu23@gmail.com>
2023-09-02 10:26:18 +0900

    feat: product update

--- Section38/Mongoose_Express/index.js
+++ Section38/Mongoose_Express/index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
-main().catch((err) => console.log(err));
+main().catch((err) => {
+  console.log("OH NO MONGO CONNECTION ERROR!!!");
+  console.log(err);
+});
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/farmStand");
-  console.log("CONNECTION OPEN!!!");
+  console.log("MONGO CONNECTION OPEN!!!");
 }
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -35,6 +41,21 @@ app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
