394dfff73fca22959ef19d2962cc502cd3a29a6e
raelulu <raelulu23@gmail.com>
2023-09-11 23:15:58 +0900

    feat: Middleware study

--- GoCamp/app.js
+++ GoCamp/app.js
@@ -53,7 +53,17 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
 });
 
 app.put("/campgrounds/:id", async (req, res) => {
-  res.send("IT WORKED!!");
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect(`/campgrounds`);
 });
 
 app.listen(3000, () => {
