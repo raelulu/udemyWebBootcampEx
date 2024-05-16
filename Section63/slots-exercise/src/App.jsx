af6772ba67a4494eb8785165832b7858c5d21156
raelulu <raelulu23@gmail.com>
2024-05-16 15:23:34 +0900

    feat: review using property

---
+++ Section63/slots-exercise/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Slots from "./Slots";
+
+function App() {
+  return (
+    <>
+      <Slots val1="🍒" val2="🍒" val3="🍒" />
+      <Slots val1="🍒" val2="🍌" val3="🍒" />
+    </>
+  );
+}
+
+export default App;
